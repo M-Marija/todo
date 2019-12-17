5f68141d12c07119a4a84444b0151446ab404b60
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-12-17 09:54:02 +0100

    Nova promena

--- todo-frontend/src/views/ListView.vue
+++ todo-frontend/src/views/ListView.vue
@@ -1,25 +1,26 @@
 <template>
-<div>
- <Navbar/>
- <List/>
-</div>
-
+  <!-- views folder sluzi da se objedini nekoliko komponenti u celoviti prikaz -->
+  <div>
+    <!--3. Renderujemo komponentu -->
+    <span></span>
+    <Navbar />
+    <List />
+  </div>
 </template>
 
 <script>
+// Redosled ukljucivanja komponentni je sledeci:
+// 1. importujemo komponentu
 import Navbar from "@/components/Navbar.vue";
 import List from "@/components/List.vue";
-
-export default{
-components:{
-Navbar,
-List
-
-}
-
-}
+export default {
+  // 2. registrujemo komponentu
+  components: {
+    Navbar,
+    List
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
